43990e03bfb1f01c56b7ad84bb2a0b35b28ecf24
z0nka1 <liang3075435@163.com>
2020-01-07 13:49:06 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-CITY=Shanghai
+CITY=Guangzhou
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
@@ -11,4 +11,4 @@ curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
+  wttr.in/$CITY
